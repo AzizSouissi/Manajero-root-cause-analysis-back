bce27fdded9f28f5d5b9e2600e3388dfe321b2e7
AzizSouissi <aziz.souissi01@gmail.com>
2024-07-04 13:32:42 +0100

    initial commit

---
+++ src/main/java/com/manajero/manajerorootcauseanalysisback/ManajeroRootCauseAnalysisBackApplication.java
@@ -0,0 +1,13 @@
+package com.manajero.manajerorootcauseanalysisback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajeroRootCauseAnalysisBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManajeroRootCauseAnalysisBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manajero/manajerorootcauseanalysisback/ManajeroRootCauseAnalysisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manajero.manajerorootcauseanalysisback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajeroRootCauseAnalysisBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
